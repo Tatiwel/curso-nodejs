678cb23d86dfa7512a3fa54e070a41265fea7869
Tatiwel <danieldandez@gmail.com>
2024-08-28 18:23:24 -0300

    Aula 3, Express
    Co-Authored-By: Marco Antonio Leal Lobão <mben.lordelo@hotmail.com>

--- index.ts
+++ index.ts
@@ -1,7 +1,47 @@
-const world = 'world';
+import express from "express";
+import { logger } from "./utils/logger";
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
+const app = express();
+
+app.use(express.json());
+
+function logger2(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  console.log("Request received");
+  next();
 }
 
-console.log(hello());
+app.use(logger);
+
+let contador = 0;
+
+function auth(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  if (req.headers.authorization) return next();
+
+  res.status(401).send("Not authorized");
+}
+
+// app.use(auth);
+
+app.get("/", (req, res) => {
+  contador++;
+  res.status(500).send(`Visitante n ${contador}`);
+});
+
+app.post("/", auth, (req, res) => {
+  console.log(req.body);
+  const a = req.body.a;
+  console.log(a);
+  res.send("POST request to the homepage");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
--- readme.md
+++ readme.md
@@ -5,10 +5,10 @@
 - [x] Basic Google search
 - [x] Basic JavaScript
 - [x] Basic TypeScript
-- [] Understanding Dependencies/Libraries and NPM (pnpm, yarn, bun, etc...)
-- [] Express Framework
-  - [] RESTful API vs (unknown name, but similar to what PHP uses)
-  - [] Middlewares
+- [x] Understanding Dependencies/Libraries and NPM (pnpm, yarn, bun, etc...)
+- [x] Express Framework
+  - [-] RESTful API vs (unknown name, but similar to what PHP uses)
+  - [x] Middlewares
 - [] Prisma
   - [] SQL vs NoSQL
   - [] PostgreSQL
